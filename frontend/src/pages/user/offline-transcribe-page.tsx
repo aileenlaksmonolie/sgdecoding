9a59ef3cebbbaef0c3e166025af1c82beab62952
thz-u <huizhantan@gmail.com>
2022-02-08 15:18:23 +0800

    to squash: partial fix

--- frontend/src/pages/user/offline-transcribe-page.tsx
+++ frontend/src/pages/user/offline-transcribe-page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Card, Container, Header } from "semantic-ui-react";
-import ModalUploadFile from '../../components/audio/upload-file-modal';
+import { Container, Header } from "semantic-ui-react";
 import styles from './offline-transcribe-page.module.scss';
 
 
@@ -10,7 +9,15 @@ const OfflineTranscribePage: React.FC = () => {
 	// template
 	return (
 		<Container id={styles.offlineTranscribePg}>
-			<Card fluid id={styles.introSection}>
+			<Container text textAlign="center" id={styles.headerContainer}>
+				<Header as="h1">Offline Transcribe</Header>
+				<p>
+					In offline transcribing, you may upload local audio files and our servers will
+					generate the text for you. The time taken for the transcription process depends
+					on the length of audio file.
+				</p>
+			</Container>
+			{/* <Card fluid>
 				<Card.Description textAlign="center">
 					<Container text>
 						<Header textAlign="center" as="h1">Offline Transcribe</Header>
@@ -18,14 +25,14 @@ const OfflineTranscribePage: React.FC = () => {
 							In offline transcribing, you may upload local audio files and our servers will
 							generate the text for you. The time taken for the transcription process depends
 							on the length of audio file.
-						</p>
-						{/* <Button color="teal">Upload Audio Files / Record Now</Button> */}
-						<ModalUploadFile>Upload Audio Files</ModalUploadFile>
+						</p> */}
+			{/* <Button color="teal">Upload Audio Files / Record Now</Button> */}
+			{/* <ModalUploadFile>Upload Audio Files</ModalUploadFile>
 					</Container>
-				</Card.Description>
-			</Card>
+				</Card.Description> */}
+			{/* </Card> */}
 
-			
+
 		</Container>
 	);
 };
