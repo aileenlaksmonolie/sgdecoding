669ba95ec96421b00bc1bc5d515e9b31b17ca9aa
thz-u <huizhantan@gmail.com>
2022-02-27 00:34:08 +0800

    refactor: files routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/auth', require('./auth.routes'));
 router.use('/speech', require('./speech.routes'));
 router.use('/users', require('./users.routes'));
+router.use('/files', require('./files.routes'));
 
 module.exports = router;
